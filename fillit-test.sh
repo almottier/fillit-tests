b0bc439afa2379cb6466345922ac2a4d17198c38
Hugo BOUILLOT <hbouillo@e1r11p19.42.fr>
2017-11-19 16:43:36 +0100

    Added best time

--- fillit-test.sh
+++ fillit-test.sh
@@ -1,5 +1,8 @@
 echo "\x1b[32;1mRéférence:\n\x1b[0m"
 time ./fillit-me $1
 echo "\n"
+echo "\x1b[32;1mMeilleur temps:\n\x1b[0m"
+time ./fillit-bst $1
+echo "\n"
 echo "\x1b[32;1mTesté:\n\x1b[0m"
 time ./fillit $1
